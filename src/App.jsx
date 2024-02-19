26ae5753a163f2cc7a5610119b49a43322da05d0
sari <sari4746@gmail.com>
2024-02-19 14:41:40 +0200

    removehref

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ function App() {
     <>
       <div>
         
-        <a href="https://react.dev" target="_blank">
+       
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       
       </div>
       <h1>Vite + React</h1>
       <div className="card">
